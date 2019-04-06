9b89f11a8768309e368a1c49c2f656ea1b3f00fb
Cory Parsnipson <cparsnipson@gmail.com>
2019-04-05 23:24:47 -0700

    Use base ReadWriteLock pointer and get rid of commented code.

--- main.cpp
+++ main.cpp
@@ -11,39 +11,10 @@
 bool go { false };
 
 int main(int argc, char** argv) {
-   //auto inc = [] (int thread_id, std::mutex* m, std::condition_variable* cv, std::shared_ptr<int> i) {
-   //   std::unique_lock<std::mutex> lck(*m);
-   //   while (!go) cv->wait(lck);
-
-   //   ++(*i);
-   //   std::cout << "Thread " << thread_id << " incrementing i. i = " << *i << std::endl;
-   //};
-
-   //std::condition_variable cv;
-
-   //std::mutex mut_i;
-   //std::shared_ptr<int> i = std::make_shared<int>(0);
-
-   //// non-initialization means thread doesn't automatically start
-   //std::thread t1;
-   //std::thread t2;
-   //
-   //// each thread starts but it gated by the wait at the beginning of inc()
-   //t1 = std::thread(inc, 1, &mut_i, &cv, i);
-   //t2 = std::thread(inc, 2, &mut_i, &cv, i);
-
-   //// set condition to true and notify everyone waiting on condition variable
-   //go = true;
-   //cv.notify_all();
-
-   //t1.join();
-   //t2.join();
-   
    std::condition_variable rw_start_cv;
    std::atomic<bool> rw_start { false };
    std::mutex rw_start_mutex;
-   //ReadWriteLock1 rwl;
-   ReadWriteLock2 rwl;
+   ReadWriteLock* rwl = new ReadWriteLock1();
 
    auto reader_f = [&rw_start_cv, &rw_start_mutex, &rw_start] (ReadWriteLock* rwl, int reader_id) {
       std::cout << "Initializing reader " << reader_id << std::endl;
@@ -93,8 +64,8 @@ int main(int argc, char** argv) {
    std::vector<std::thread> writers;
 
    for (int i = 0; i < 10; ++i) {
-      readers.push_back(std::thread(reader_f, &rwl, i));
-      writers.push_back(std::thread(writer_f, &rwl, i));
+      readers.push_back(std::thread(reader_f, rwl, i));
+      writers.push_back(std::thread(writer_f, rwl, i));
    }
 
    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
